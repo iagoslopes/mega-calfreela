e1f143df4da7a3f8f1b3d73e71654c7d3f8095bb
Iago <iagodesouza2012@gmail.com>
2023-12-07 19:28:28 -0300

    add: componentes do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CalculatorContainer from './componentes/CalculatorContainer';
 import Header from './componentes/Header';
 import InfoBar from './componentes/InfoBar';
 import Footer from './componentes/Footer';
@@ -10,6 +11,9 @@ function App() {
       <InfoBar
         title="Para você que é MEI ou quer se tornar MEI, calcule o valor da sua hora de trabalho:"
       />
+
+
+      <CalculatorContainer />
 
       <InfoBar
         title="Calcule o valor a ser cobrado para um projeto:"
